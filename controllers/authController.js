0d956ac63c1d82ed8931e74d9c3f6359638498ed
Paul Sink <SinkPaulD@JohnDeere.com>
2020-12-15 17:03:16 -0500

    add userID and admin to res.json

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,7 +70,11 @@ router.post("/login", (req, res) => {
           console.log(token);
           // res.cookie("jwt", token); // SEND A NEW COOKIE TO THE BROWSER TO STORE TOKEN
           // res.redirect(`/users/profile/${foundUser.id}`);
-          res.json({ token });
+          res.json({
+            token: token,
+            id: foundUser.id,
+            admin: foundUser.admin
+          });
         } else {
           return res.sendStatus(400);
         }
